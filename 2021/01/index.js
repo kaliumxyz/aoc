4fd267063730f6f358d1455ff9dc8dee7d341d83
kalium <kalium@kalium.xyz>
2021-12-02 23:37:23 +0100

    day 1-2

--- 2021/01/index.js
+++ 2021/01/index.js
@@ -1,14 +1,31 @@
 #!/usr/bin/env node
 fs = require("fs");
 
-const input = fs.readFileSync("./input", "utf8").split("\n")
 
-let inc = 0;
+fs.readdirSync("input").forEach(file => {
+  console.log(file);
+  const input = fs.readFileSync(`input/${file}`, "utf8").split("\n")
 
-input.forEach((x,i) => {
-  if(x - input[i-1] > 0) {
-    inc++
-  }
-})
+  let inca = 0;
+  let incb = 0;
 
-console.log('part 1:', inc)
+  input.forEach((x,i) => {
+    if(x - input[i-1] > 0) {
+      inca++
+    }
+
+    if(i < (input.length - 3)) {
+      const a = +x + +input[i+1] + +input[i+2]
+      const b = +input[i+1] + +input[i+2] + +input[i+3]
+      if (file == "test") {
+        console.log(a,b)
+      }
+      if (a - b < 0) {
+        incb++
+      }
+    }
+  })
+
+  console.log('part 1:', inca)
+  console.log('part 2:', incb)
+});
