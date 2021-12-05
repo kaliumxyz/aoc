fd660f3c607d454261d4e3380e564e5fd5fa4361
kalium <kalium@kalium.xyz>
2021-12-05 05:25:49 +0100

    day 3-2

--- 2021/03/index.js
+++ 2021/03/index.js
@@ -6,58 +6,60 @@ fs.readdirSync("input").forEach(file => {
   console.log(file);
   const input = fs.readFileSync(`input/${file}`, "utf8").split("\n")
   const acc = [];
-  let len = 0
+  const len = input[0].length;
 
-  let ox = []
-  let co = []
-
-  let oxa = []
-  let coa = []
+  let ox = [...input]
+  let co = [...input]
 
   input.forEach((line,j) => {
     if (line !=="") {
-      acc[j] = 0;
       line.split("").forEach((x,i) => {
         if (x == "1")
-          acc[j] += 1;
+          acc[i] = acc[i]?acc[i]+1:1;
       })
       if (file == "test") {
         console.log(line)
       }
-      len = line.length;
     }
   });
   const gamma = acc.map(x => x > input.length/2 ? 1 : 0)
   const epsilon = acc.map(x => x < input.length/2 ? 1 : 0)
   console.log(parse(gamma) * parse(epsilon))
-  //console.log(gamma, epsilon)
-    input.forEach((line,j) => {
-      oxa[j] = line;
-      coa[j] = line;
-
-      if (line !== "") {
-        ox[j] = 0
-        co[j] = 0
-        line.split("").forEach((x,i) => {
-      if (file == "test") {
-          if (x == gamma[i]) {
-            ox[j] += +1;
-            oxa[j] = false;
-          }
-          if (x == epsilon[i]) {
-            co[j] += +1;
-            coa[j] = false;
-          }
-        }
-      })
+  for(let i = 0; i < len; i++){
+    const lenn = ox?ox.length:0;
+    num = ox.reduce((acc, x) => acc + (x[i]?+x[i]:0), 0)
+    bit = num >= (lenn/2)
+    ox = ox.filter(x =>
+      x[i] == bit
+    )
+    if (file == "test") {
+      console.log(lenn,num, bit)
+      console.log(ox)
     }
-  });
-  const a = ox.indexOf(Math.max(...ox));
-  const b = co.indexOf(Math.max(...co));
+    if (ox.length == 1)
+      break;
+  }
 
-  console.log(coa, oxa)
-  console.log(parseInt(input[a],2) * parseInt(input[b],2), a, b)
+  for(let i = 0; i < len; i++){
+    const lenn = co?co.length:0;
+    num = co.reduce((acc, x) => acc + (x[i]?+x[i]:0), 0)
+    bit = num < (lenn/2)
+    co = co.filter(x =>
+      x[i] == bit
+    )
+    if (file == "test") {
+      console.log(lenn,num, bit)
+      console.log(co)
+    }
+    if (co.length == 1)
+      break;
+  }
+  console.log(sparse(ox[0]) * sparse(co[0]))
 });
+
+function sparse(arr) {
+  return parse(arr.split(''))
+}
 
 function parse(arr) {
   return parseInt(arr.join(""), 2)
